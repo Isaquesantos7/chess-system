0b9a7d42ad9fd9a56f5d2e28cd19983fd33c35f2
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2022-05-17 18:40:36 -0300

    starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,9 @@
 package application;
 
-import boardGame.Position;
+import boardGame.Board;
 
 public class Program {
 	public static void main(String[] args) {
-		var pos = new Position(3,5);
-
-		System.out.println(pos);
+		var board = new Board(8,8);
 	}
 }
