4d809d0331e2a84197cd43b27676271fd38d79b3
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2022-05-20 11:29:07 -0300

    Knigth

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,7 @@ import boardGame.Piece;
 import boardGame.Position;
 import chess.piece.Bishop;
 import chess.piece.King;
+import chess.piece.Knigth;
 import chess.piece.Pawn;
 import chess.piece.Rook;
 
@@ -74,7 +75,7 @@ public class ChessMatch {
             throw new ChessException("You can't put yourself in check!");
         }
 
-        check = ((testCheck(opponent(currentPlayer))) ? true : false);
+        check = (testCheck(opponent(currentPlayer))) ? true : false;
 
         if(testCheckMate(opponent(currentPlayer))){
             checkMate = true;
@@ -162,32 +163,30 @@ public class ChessMatch {
         return false;
     }
 
-    private static boolean testCheckMate(Color color){
-        if(!testCheck(color)){
-            return false;
-        }
-        
-        List<Piece> list = capturedPieces.stream().filter(X -> ((ChessPiece)X).getColor() == color).collect(Collectors.toList());
-        
-        for(Piece p : list){
-            boolean[][] mat = p.possibleMoves();
-            for(int i = 0; i < board.getRows(); i++){
-                for(int j = 0; i < board.getRows(); j++){
-                    if(mat[i][j]){
-                        Position source = ((ChessPiece)p).getChessPosition().toPosition();
-                        Position target = new Position(i, j);
-                        Piece capturedPiece = makeMove(source, target);
-                        boolean testCheck = testCheck(color);
-                        undoMove(source, target, capturedPiece);
-                        if(!testCheck){
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-        return true;
-    }
+    private static boolean testCheckMate(Color color) {
+		if (!testCheck(color)) {
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+		for (Piece p : list) {
+			boolean[][] mat = p.possibleMoves();
+			for (int i=0; i<board.getRows(); i++) {
+				for (int j=0; j<board.getColumns(); j++) {
+					if (mat[i][j]) {
+						Position source = ((ChessPiece)p).getChessPosition().toPosition();
+						Position target = new Position(i, j);
+						Piece capturedPiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturedPiece);
+						if (!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
@@ -196,9 +195,11 @@ public class ChessMatch {
 
     private void initialSetup(){
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('b', 1, new Knigth(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('g', 1, new Knigth(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -210,9 +211,11 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('b', 8, new Knigth(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('g', 8, new Knigth(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
